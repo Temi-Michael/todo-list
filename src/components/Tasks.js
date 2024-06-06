419cb776d2a5691c09d0bccbaf388c0b6a20629e
Temi Michael <olasehindemichael285@gmail.com>
2024-06-06 11:14:05 +0100

    responsiveness and key event add

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,47 +1,71 @@
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react";
 
 export default function Tasks() {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const inputRef = useRef(null);
 
-    const [input, setInput] = useState('')
-    const [list, setList] = useState([]);
+  const enterInput = (e) => {
+    if(e.key === 'Enter') {
+        handleAdd()
+    }
+  }
 
-    const handleAdd = () => {
-        const newTask = {
-            id: Math.random(),
-            todo: input
-        };
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
-        setList([...list, newTask]);
-        setInput('');
+  const handleAdd = () => {
+    const newTask = {
+      id: Math.random(),
+      todo: input,
     };
+    if (input.length > 3) {
+      setList([...list, newTask]);
+      setInput("");
+    } else {
+      alert(
+        `Make sure your task item has more than 3 words to easily identify task.`
+      );
+    }
+    inputRef.current.focus();
+  };
 
-    const handleDelete = (id) => {
-        const newList = list.filter((todo) => todo.id !== id);
-        setList(newList);
-    };
+  const handleDelete = (id) => {
+    const newList = list.filter((todo) => todo.id !== id);
+    setList(newList);
+  };
 
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
 
+  return (
+    <div className="todo">
+      <h1>TO-DO LISTS</h1>
+      <input
+        className="input"
+        type="text"
+        value={input}
+        ref={inputRef}
+        onChange={handleInputChange}
+        onKeyDown={enterInput}
+        placeholder="Add Tasks...."
+      />
+      <button className="btn" onClick={handleAdd}>
+        ADD
+      </button>
 
-    const handleInputChange = (event) => {
-        setInput(event.target.value);
-    };
-
-    return (
-        <div className="todo">
-            <h1>TO-DO LISTS</h1>
-            <input className="input" type="text" value={input} onChange={handleInputChange} placeholder="Add Tasks...." />
-            <button className="btn" onClick={handleAdd}>ADD</button>
-
-            <ul>
-                {list.map((todo) => (
-                    <li className="task" key={todo.id}>
-                        {todo.todo}
-                        <button onClick={() => handleDelete(todo.id)}>
-                            Delete
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </div >
-    )
-};
+      <ul>
+        {list.map((todo) => (
+          <li className="task" key={todo.id}>
+            {todo.todo}
+            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
